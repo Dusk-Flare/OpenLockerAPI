70683938c655f0f406206713f6b7022e5bcbd913
DuskFlare13 <Redstoner113@gmail.com>
2026-08-07 11:19:26 -0300

    initialization

---
+++ OpenLockerLib/Logic.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenLockerLib.Utils;
+using UnityEngine;
+
+namespace OpenLockerLib
+{
+    public static class Logic
+    {
+        private static float updateTime = -1f;
+
+        private static List<StorageContainer> storages = new();
+        public static void RefreshLocalStorage(float range = 30f)
+        {
+            StorageContainer[] containers = Object.FindObjectsOfType<StorageContainer>();
+            Vector3 player = Player.main.transform.position;
+            List<StorageContainer> localStorage = containers.ToList().FindAll(s => Vector3.Distance(player, s.transform.position) <= range);
+            storages = localStorage;
+            updateTime = Time.time;
+        }
+
+        public static int GetLocalPickupCount(TechType type)
+        {
+            if (Time.time - updateTime <= 1) RefreshLocalStorage();
+            return storages.Sum(s => s.container.GetCount(type));
+        }
+
+        public static List<StorageResource> GetStorageResources(StorageContainer container)
+        {
+            List<StorageResource> resources = new();
+            foreach (TechType type in container.container.GetItemTypes())
+            {
+                resources.Add(new StorageResource(container, type));
+            }
+            return resources;
+        }
+
+        public static List<StorageResource> GetAllStorageResources()
+        {
+            List<StorageResource> resources = new();
+            foreach (StorageContainer container in storages)
+            {
+                List<StorageResource> containerResources = GetStorageResources(container);
+                foreach (TechType type in container.container.GetItemTypes())
+                {
+                    int total = containerResources.FindAll(r => r.Type == type).Sum(re => re.PickupCount);
+                    resources.Add(new StorageResource(container, new Resource(type, total)));
+                }
+            }
+            return resources;
+        }
+
+        public static bool ConsumeLocalResource(TechType type, int amount)
+        {
+            if (Time.time - updateTime <= 1) RefreshLocalStorage();
+            if(GetLocalPickupCount(type) < amount) return false;
+            foreach(StorageContainer container in storages)
+            {
+                if(amount <= 0) break;
+                if (container.container.GetCount(type) <= 0) continue;
+                int consume = Mathf.Min(amount, container.container.GetCount(type));
+                amount -= ConsumeResource(new(container, new Resource(type, consume))) ? consume : 0;
+            }
+            return amount <= 0;
+        }
+
+        public static bool ConsumeResource(StorageContainer container, TechType type, int amount) => ConsumeResources(new List<StorageResource> { new(container, new Resource(type, amount)) });
+
+        public static bool ConsumeResources(List<(StorageContainer container, TechType type, int amount)> resourceList) => ConsumeResources(resourceList.Select(r => new StorageResource(r.container, new Resource(r.type, r.amount))).ToList());
+
+        public static bool ConsumeResource(StorageResource resource) => ConsumeResources(new List<StorageResource> { resource });
+
+        public static bool ConsumeResources(List<StorageResource> resources)
+        {
+            foreach (StorageResource removal in resources)
+            {
+                List<InventoryItem> items = removal.Container.container.GetItems(removal.Type).ToList();
+                Resource resource = removal.Resource;
+                foreach (InventoryItem item in items)
+                {
+                    if (resource <= 0) break;
+                    Pickupable pickupable = item.item;
+
+                    removal.Container.container.RemoveItem(pickupable, false);
+                    resource -= 1;
+                }
+            }
+            return resources.All(r => r.Resource.Amount <= 0);
+        }
+    }
+}
---
+++ OpenLockerLib/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using OpenLockerLib.Utils;
+using HarmonyLib;
+using System.Reflection;
+
+namespace OpenLockerLib
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static Mercury Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        private void Awake()
+        {
+            // plugin startup logic
+            Logger = new Mercury(PluginInfo.PLUGIN_NAME);
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
